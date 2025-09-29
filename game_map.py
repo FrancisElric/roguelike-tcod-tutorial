77e92fbb850c35f7a92702536a1586f4c3c06772
Francis Elric <mieszko.sadek@gmail.com>
2025-09-29 15:06:09 +0200

    - started part 4 of tutorial -- added light (visible) tiles and a default SHOROUD (unseen) tile - also added some comments for future me

--- game_map.py
+++ game_map.py
@@ -8,12 +8,34 @@ class GameMap:
     def __init__(self, width: int, height: int):
         self.width, self.height = width, height
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
+        # Default value of a tile is to just be a walll
+        self.visible = np.full((width, height), fill_value=False, order="F")
+        # Tiles the player can currently see
+        self.explored = np.full((width, height), fill_value=False, order="F")
+        # Tiles the player has seen before
 
-        self.tiles[30:33, 22] = tile_types.wall
+        # Context - These are 2d arrays that hold all tiles in our map
 
     def in_bounds(self, x: int, y: int) -> bool:
         """Return True if x and y are inside of the bounds of this map."""
         return 0 <= x < self.width and 0 <= y < self.height
 
     def render(self, console: Console) -> None:
-        console.tiles_rgb[0:self.width, 0:self.height] = self.tiles["dark"]
+        """
+                Renders the map.
+
+                If a tile is in the "visible" array, then draw it with the "light" colors.
+                If it isn't, but it's in the "explored" array, then draw it with the "dark" colors.
+                Otherwise, the default is "SHROUD".
+                """
+        console.tiles_rgb[0:self.width, 0:self.height] = np.select(
+            condlist=[self.visible, self.explored],
+            choicelist=[self.tiles["light"], self.tiles["dark"]],
+            default=tile_types.SHROUD
+        )
+
+
+        # dobra zmęczyłem sb
+        # ostania linijka na której jestem to "With all that in place, we need..."
+        # później zrb resztę, na razie wszystko jest ciemne (SHROUD) bo nie dodaliśmy logiki FOV
+        # i nic się nie zmienia w widoczne
